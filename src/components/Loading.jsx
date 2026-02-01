ef6384074ad614def22196d14d75f85babecfe44
AtulMishra001 <atulm7151@gmail.com>
2026-02-02 01:27:34 +0530

    added 404 not found page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="width-[100vw] h-screen flex justify-center items-center">
+    <div className="width-[100vw] h-[50vh] flex justify-center items-center">
         <div
         className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,20 +2,25 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-4xl font-bold text-red-600 mb-4">
-        404 - Page Not Found
-      </h1>
-      <p className="text-gray-600 mb-6">
-        Oops! The page you are looking for does not exist.
-      </p>
-      <Link
-        to="/"
-        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-      >
-        Go Back Home
-      </Link>
-    </div>
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-400">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Go back home
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 };
 export default NotFound;
